bf457f805fb3d5734ad2b32a8accc6450fbd121e
aparipip <arifnc6@gmail.com>
2025-06-16 17:47:36 +0700

    fix: navbar.php

--- public/templates/navbar.php
+++ public/templates/navbar.php
@@ -1,6 +1,6 @@
 <?php
                                                         // Ambil path root aplikasi (sesuaikan sesuai project-mu)
-    $baseUrl        = '/pemrograman-web/SIBAS_WebApp/'; // TANPA titik!
+    $baseUrl        = '/sibas-webapp/'; // TANPA titik!
     $defaultProfile = $baseUrl . 'vendor/img/undraw_profile.svg';
 
     // Set profile
@@ -194,19 +194,6 @@
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                                 aria-labelledby="userDropdown">
-                                <a class="dropdown-item" href="#">
-                                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Profile
-                                </a>
-                                <a class="dropdown-item" href="#">
-                                    <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Settings
-                                </a>
-                                <a class="dropdown-item" href="#">
-                                    <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Activity Log
-                                </a>
-                                <div class="dropdown-divider"></div>
                                 <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
                                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Logout
